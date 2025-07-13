6de96cc3eb6ade7c018ac073bcfdb953f7b3a002
Dipendra Bhandari <bhandaridipendra94@gmail.com>
2025-07-13 18:04:49 +1000

    Updating website name

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,25 @@
 import { Text } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import { Great_Vibes } from "next/font/google";
+
+const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const Header = () => {
   return (
-    <div className="flex h-[12vh] items-center p-2 bg-neutral">
+    <div className="flex h-[15vh] items-center p-2 bg-neutral">
       <div className="pl-2">
         <div className="flex flex-col">
           <Link
             href="/"
-            className="text-3xl md:text-5xl lg:text-5xl  text-white hover:text-orange-400 transition-colors
-      
-      
-      "
+            className={`text-3xl ${greatVibes.className} md:text-5xl lg:text-5xl  text-white hover:text-orange-400 transition-colors`}
           >
-            Dipendra Bhandari
+            The Coding Vibes
           </Link>
-          <Text className="text-slate-100">Senior Software Engineer</Text>
+          <Text className="text-slate-100"><span className="text-xs italic tracking-widest">with</span> Dipendra Bhandari</Text>
         </div>
       </div>
     </div>
